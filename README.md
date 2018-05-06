7196c1088cd6df7b34e3f1ef37b0e04afac743c6
Nathan Glover <nathan@glovers.id.au>
2018-05-06 18:09:26 +0800

    encrypted variables

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ Handles pushing messages out to a Slack channel as ButlerBot
 
 Setup you Slack bot and create a Bot User. within your AWS Lambda function panel add two environment variables
 
-* **slack_token** : This is the Bot api token that is given to you when you create the Bot user.
+* **slackbot_encrypted_token** : This is the Bot api token that is given to you when you create the Bot user, this token should have been setup in KMS and passed as an encrypted variable.
+
 * **slackbot_name** : This is your Bot User name
+
+* **slackbot_channel** : This is the channel your bot with post to.
--- index.js
+++ index.js
@@ -2,30 +2,41 @@
 const Slackbot = require('slackbots');
 const AWS = require('aws-sdk');
 
-const slack_token = process.env['slack_token'];
 const slackbot_name = process.env['slackbot_name'];
-
-var channel = 'general';
+const slackbot_encrypted_token = process.env['slackbot_token'];
+const slackbot_channel = process.env['slackbot_channel'];
+let slackbot_decrypted_token;
 
 exports.handler = (event, context, callback) => {
-    if (slack_token && slackbot_name) {
+    if (slackbot_decrypted_token && slackbot_name) {
         processEvent(event, context, callback);
+    } else {
+        const kms = new AWS.KMS();
+        kms.decrypt({ CiphertextBlob: new Buffer(slackbot_encrypted_token, 'base64') }, (err, data) => {
+            if (err) {
+                console.log('Decrypt error:', err);
+                return callback(err);
+            }
+            slackbot_decrypted_token = data.Plaintext.toString('ascii');
+            processEvent(event, context, callback);
+        });
     }
 };
 
 function processEvent(event, context, callback) {
     var butlerbot = new Slackbot({
-        token: slack_token,
+        token: slackbot_decrypted_token,
         name: slackbot_name
     });
     console.log('Received event:', event);
+    const payload = JSON.stringify(event);
 
-    butlerbot.postMessageToChannel(channel, "", {
+    butlerbot.postMessageToChannel(slackbot_channel, "", {
         as_user: 'true',
         attachments: [{
             title: "Incoming Message!",
             fallback: "Message Receieved",
-            text: event,
+            text: payload,
             color: "#000000"
         }]
     }).then(function(data) {
